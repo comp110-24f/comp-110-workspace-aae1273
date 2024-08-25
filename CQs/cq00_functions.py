068c43907ff7eb076f4c3cd53b1fefc5df7d8e21
aae1273 <ael-halabi@unc.edu>
2024-08-25 16:21:42 -0400

    Finished CQ00 !

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,11 @@
+__author__ = "730474650"
+
+
+def mimic(message: str) -> str:
+    return str
+
+
+"""The function should simply return message back to you!"""
+
+if __name__ == "__main__":
+    print(mimic(message=input("What is your message?")))
